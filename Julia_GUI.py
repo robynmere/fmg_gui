451728d01554a9eec58788097bd1ab46ab3eb915
julsmacp <julia.macpherson@unb.ca>
2024-01-18 17:46:33 -0400

    Update Julia_GUI.py
    Basic code

---
+++ Julia_GUI.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+root = Tk()
+
+#creating main window
+window_title = Label(root, text = "FMG Wearable Device")
+window_title.grid(row = 0, column = 3)
+
+
+#Inputs
+L1 = Label(root, text = "Display length for FMG data display ")
+L1.grid(row = 1, column = 0)
+display_length= Entry(root)
+display_length.grid(row = 1, column = 1)
+
+L2 = Label(root, text = "Maximum file size for recording ")
+L2.grid(row = 2, column = 0)
+max_file = Entry(root)
+max_file.grid(row = 2, column = 1)
+
+#adding a button that exits the program
+button_quit = Button(root, text = " Exit Program", command = root.quit)
+button_quit.grid(row = 5, column = 5)
+
+def record_click(): #creating a function for the button to call
+    myLabel = Label(root, text="this function represents recording data")
+    myLabel.grid(row = 5, column = 4)
+
+record_data= Button(root, text="Record Data", command=record_click) #no paranthese on myclick
+record_data.grid(row = 4, column = 4)
+
+root.mainloop()
