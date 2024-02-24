06ec535d17d3eed65a12ba1a7676125ff193e1c8
leah-morrison <leah.morrison@unb.ca>
2024-02-24 11:45:32 -0400

    Countdown
    Added countdown to training and gestures - work in progress.

--- gestures.py
+++ gestures.py
@@ -9,6 +9,7 @@ root = tk.Tk()
 root.title('Machine Learning Gestures') 
 root.geometry('{}x{}'.format(1000,1000))
 
+# defining functions
 def forward():  
     global j # creating a global variable j  
     j = j + 1  
@@ -28,8 +29,19 @@ def backward():
         name_label.config(text = name_list[j])
     except:  
         j = 0  
-        backward() # calling the forward function 
+        backward() # calling the backward function 
 
+def countdown(count):       
+    count_label["text"] = count
+
+    if count > 0:
+        root.after(1000, countdown, count-1) #call countdown after 1000ms (1s)
+    
+    else:
+        countDone = tk.Label(root, text = "Click 'Next'.")
+        countDone.grid(row = 3, column = 5)
+
+# adding buttons
 back_button = Button(root, text = 'Back', command = backward)
 back_button.grid(row=0, column=0)
     
@@ -46,16 +58,20 @@ image_up = ImageTk.PhotoImage(Image.open("Images/Thumbs_Up.png").resize((500, 50
 image_ext = ImageTk.PhotoImage(Image.open("Images/Wrist_Extension.png").resize((500, 500)))
 image_flex = ImageTk.PhotoImage(Image.open("Images/Wrist_Flexion.png").resize((500, 500)))
 
-# listing images and names
+# listing images and their names
 image_list = [image_no, image_chuck, image_open, image_close, image_down, image_up, image_ext, image_flex]
 name_list = ["No Motion","Chuck Grip","Hand Open","Hand Closed","Thumbs Down","Thumbs Up","Wrist Extension","Wrist Flexion"]
 j = 0  
 img_label = Label(root, image = image_list[j])
 name_label = Label(root, text = name_list[j])
 
+# countdown
+count_label = tk.Label(root)
+countdown(5)
+
 # grid controls
 img_label.grid(row=0, column=2)
 name_label.grid(row=1, column=2)
-
+count_label.grid(row = 2, column = 2)
 
 root.mainloop()
--- train.py
+++ train.py
@@ -6,14 +6,28 @@ from matplotlib.backends.backend_tkagg import(FigureCanvasTkAgg, NavigationToolb
 import sys
 import os
 
-#creating main window
+# creating main window
 root = tk.Tk() 
 root.title('FMG Wearable Training Module') #window title
 root.geometry('{}x{}'.format(1000,1000))
 
+# creating countdown
+def countdown(count):       
+    countLabel["text"] = count
+
+    if count > 0:
+        root.after(1000, countdown, count-1) #call countdown after 1000ms (1s)
+    else:
+        countDone = tk.Label(root, text = "Click 'Next'.")
+        countDone.grid(row = 3, column = 5)
+
+countLabel = tk.Label(root)
+countLabel.grid(row = 3, column = 5)
+countdown(5)
+
 #creating input widgets and labels
 L1 = Label(root, text = "This is the training window")
-L1.grid(row=1, column=0, sticky="SW")
+L1.grid(row=1, column=5, sticky="SW")
 
 #exit button
 button_quit = Button(root, text = "Exit", command = root.quit)
