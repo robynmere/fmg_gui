9940918231cf9e00c6df58d50ac21b719c88888b
leah-morrison <leah.morrison@unb.ca>
2024-02-24 10:24:07 -0400

    Update gestures.py

--- gestures.py
+++ gestures.py
@@ -4,7 +4,7 @@ import sys
 import os
 from PIL import ImageTk, Image  
 
-#creating main window
+# creating main window
 root = tk.Tk() 
 root.title('Machine Learning Gestures') 
 root.geometry('{}x{}'.format(1000,1000))
@@ -36,6 +36,7 @@ back_button.grid(row=0, column=0)
 forward_button = Button(root, text = 'Next', command = forward)
 forward_button.grid(row=0, column = 3)
   
+# images
 image_no = ImageTk.PhotoImage(Image.open("Images/No_Motion.png").resize((500, 500)))
 image_chuck = ImageTk.PhotoImage(Image.open("Images/Chuck_Grip.png").resize((500, 500)))
 image_open = ImageTk.PhotoImage(Image.open("Images/Hand_Open.png").resize((500, 500)))
@@ -45,12 +46,15 @@ image_up = ImageTk.PhotoImage(Image.open("Images/Thumbs_Up.png").resize((500, 50
 image_ext = ImageTk.PhotoImage(Image.open("Images/Wrist_Extension.png").resize((500, 500)))
 image_flex = ImageTk.PhotoImage(Image.open("Images/Wrist_Flexion.png").resize((500, 500)))
 
+# listing images and names
 image_list = [image_no, image_chuck, image_open, image_close, image_down, image_up, image_ext, image_flex]
 name_list = ["No Motion","Chuck Grip","Hand Open","Hand Closed","Thumbs Down","Thumbs Up","Wrist Extension","Wrist Flexion"]
 j = 0  
-img_label = Label(root, image = image_list[j]) 
-img_label.grid(row=0, column=2)
+img_label = Label(root, image = image_list[j])
 name_label = Label(root, text = name_list[j])
+
+# grid controls
+img_label.grid(row=0, column=2)
 name_label.grid(row=1, column=2)
 
 
