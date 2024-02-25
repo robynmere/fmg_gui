cf76878d93b352e9bbbdd5a5c23cc8ad81bce768
leah-morrison <leah.morrison@unb.ca>
2024-02-24 22:58:42 -0400

    WIP
    Trying to make select all and clear all buttons - I can do it in a separate file but not here for some reason.

--- user_config.py
+++ user_config.py
@@ -7,7 +7,7 @@ from matplotlib.backends.backend_tkagg import(FigureCanvasTkAgg, NavigationToolb
 import sys
 import os
 
-# input from main.py - NEED TO CHANGE
+# input from main.py - NEED TO CHANGE TO READ FROM main.py
 number_fsrs = 6
 
 # creating main window
@@ -15,8 +15,16 @@ root = tk.Tk()
 root.title('Machine Learning Configuration') #window title
 #root.geometry('{}x{}'.format(800,500))
 
+# defining functions
+def clear_all(listbox,list):
+    listbox.selection_clear(0,len(list))
+
+def select_all(listbox,list):
+    listbox.selection_set(0,len(list))
+
+
 # choosing which sensors to read
-sensor_label = Label(root, text = "Please select which sensors to read:")
+sensor_label = Label(root, text = "Please select which sensors to read (scroll for full selection):")
 sensor_label.pack(expand = True, side = tk.TOP)
 
 if number_fsrs == 6:
@@ -29,17 +37,26 @@ sensor_listbox = tk.Listbox(root, listvariable = sensor_items, height = 6, selec
 sensor_listbox.pack(expand = True, fill = tk.BOTH, side = tk.TOP)
 
 def sensor_selection():
-    name_index = name_listbox.curselection()
-    selected_names = ", ".join([name_listbox.get(i) for i in name_index])
-    selection_text = "You have selected " + selected_names + "."
+    sensor_index = sensor_listbox.curselection()
+    selected_sensors = ",".join([sensor_listbox.get(i) for i in sensor_index])
+    selection_text = "You have selected to train the following sensors:" + selected_sensors
     selection_label = Label(root, text = str(selection_text))
-    selection_label.pack(expand = True, side = tk.BOTTOM)
+    selection_label.pack(expand = True, side = tk.TOP)
+
+'''
+clear_button1 = Button(root, text = 'Clear All', command = clear_all(sensor_listbox,sensor_list))
+clear_button1.pack(expand = True)
+
+all_button1 = Button(root, text = 'Select All', command = select_all(sensor_listbox,sensor_list))
+all_button1.pack(expand = False)
+'''
 
 confirm_button1 = Button(root, text = '  Confirm Selection  ', command = sensor_selection)
-confirm_button1.pack(expand = True, side = tk.BOTTOM)
+confirm_button1.pack(expand = True)
+
 
 # choosing gestures
-gesture_label = Label(root, text = "Please select all gestures to train:")
+gesture_label = Label(root, text = "Please select all gestures to train (scroll for full selection):")
 gesture_label.pack(expand = True, side = tk.TOP)
 
 name_list = ["No Motion","Chuck Grip","Hand Open","Hand Closed","Thumbs Down","Thumbs Up","Wrist Extension","Wrist Flexion"]
@@ -50,13 +67,13 @@ name_listbox.pack(expand = True, fill = tk.BOTH, side = tk.TOP)
 def name_selection():
     name_index = name_listbox.curselection()
     selected_names = ", ".join([name_listbox.get(i) for i in name_index])
-    selection_text = "You have selected " + selected_names + "."
+    selection_text = "You have selected the following gestures to train: " + selected_names
     selection_label = Label(root, text = str(selection_text))
     selection_label.pack(expand = True, side = tk.BOTTOM)
 
 # buttons
 confirm_button2 = Button(root, text = '  Confirm Selection  ', command = name_selection)
-confirm_button2.pack(expand = True, side = tk.BOTTOM)
+confirm_button2.pack(expand = True, side = tk.TOP)
 
 '''
 # other
